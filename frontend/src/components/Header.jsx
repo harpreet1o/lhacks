9b1892a41eccc108ec3bdfebb1bbce65637f30c8
Grace Barros <gracepbarros@gmail.com>
2024-09-21 14:55:58 -0700

    Design with Bootstrap

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="text-center mb-4">
+      <h1 className="fw-bold text-success">EasyFit</h1>
+      <div className="bg-success mt-20 strip"></div>
+    </div>
+
+  );
+};
+
+export default Header;
