f92de8a74f012f4f3151335058eedfc6cb15e498
Harpreet Singh <harpreetx01@outlook.com>
2024-09-21 10:41:55 -0700

    added the mongodb

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 import logger from "morgan";
 import indexRouter from "./routes/workout.js"
+import mongoose from "mongoose";
+import 'dotenv/config';
+
+
 
 const app=express();
 app.use(cors());
@@ -10,6 +14,12 @@ app.use(logger("dev"));
 app.get("/",(req,res)=>{
 res.json("hello from the server side");
 })
+async function connectToDatabase() {
+    const uri =process.env.mongodbUrl;
+    await mongoose.connect(uri);
+    console.log("mongodb connected")
+  }
+  connectToDatabase();
 app.use("/",indexRouter);
 
 app.listen(3000,()=>{
