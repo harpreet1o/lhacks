389071cc7a0f0aaa0c92f46d92bc1ba9065f0cd7
Grace Barros <gracepbarros@gmail.com>
2024-09-21 21:00:59 -0700

    exercise card draft

--- frontend/src/components/Exercise.jsx
+++ frontend/src/components/Exercise.jsx
@@ -1,9 +1,10 @@
-const ExerciseCard = ({name, rep, }) => {
+const ExerciseCard = ({name, rep, series}) => {
   return (
     <div className="card">
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
         <p className="card-text">Reps: {rep}</p>
+        <p className="card-text">Series: {series}</p>
       </div>
     </div>
   );
--- frontend/src/pages/Workout.jsx
+++ frontend/src/pages/Workout.jsx
@@ -1,9 +1,14 @@
 import Header from "../components/Header";
+import ExerciseCard from "../components/Exercise";
 
 function Workout() {
   return (
     <div className="">
       <Header />
+      <div className="container-fluid text-center mb-4">
+        <h3>Workout</h3>
+      <ExerciseCard name="Push-ups" rep="10" series="3" />
+    </div>
     </div>
   );
 }
