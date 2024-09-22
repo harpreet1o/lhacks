41bb4bb2256650e829157df9b3e6f13c16a1ee73
Grace Barros <gracepbarros@gmail.com>
2024-09-21 21:12:00 -0700

    finising merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home'
+import Workout from './pages/Workout'
 import Setup from './pages/Setup'
 import Workout from './pages/Workout'
 import Navbar from './component/Navbar';
 import Home from './component/Home';
 import Login from './component/Login';
 import Signup from './component/Signup';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   
 
   return (
-    <>
-     
-     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home/>} />
-        <Route path="/signup" element={<Signup/>}/>
-        
-
-      </Routes>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/workout" element={<Workout />} />
+          <Route path="/setup" element={<Setup />} />
+        </Routes>
     </Router>
-    </>
   )
 }
 
